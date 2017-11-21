04ae02361db94691831f1134ec068587ab56a7a5
THIZY Kevin <Nakasar@users.noreply.github.com>
2017-11-21 20:09:01 +0100

    Fixed parsing of formatting.

--- components/base.js
+++ components/base.js
@@ -25,13 +25,13 @@ class User {
 function formatText(text) {
   var formatted = "";
 
-  formatted = text.replace(/\[color=(.+)\](.+)\[\/color\]/g, '<span style="color: $1;">$2</span>');
-  formatted = formatted.replace(/\[b\](.+)\[\/b\]/g, '<strong>$1</strong>');
-  formatted = formatted.replace(/\*\*(.+)\*\*/g, '<strong>$1</strong>');
-  formatted = formatted.replace(/\[i\](.+)\[\/i\]/g, '<em>$1</em>');
-  formatted = formatted.replace(/\*(.+)\*/g, '<em>$1</em>');
-  formatted = formatted.replace(/\[u\](.+)\[\/u\]/g, '<u>$1</u>')
-  formatted = formatted.replace(/_(.+)_/g, '<u>$1</u>')
+  formatted = text.replace(/\[color=(.+?)\](.+?)\[\/color\]/g, '<span style="color: $1;">$2</span>');
+  formatted = formatted.replace(/\[b\](.+?)\[\/b\]/g, '<strong>$1</strong>');
+  formatted = formatted.replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>');
+  formatted = formatted.replace(/\[i\](.+?)\[\/i\]/g, '<em>$1</em>');
+  formatted = formatted.replace(/\*(.+?)\*/g, '<em>$1</em>');
+  formatted = formatted.replace(/\[u\](.+?)\[\/u\]/g, '<u>$1</u>')
+  formatted = formatted.replace(/_(.+?)_/g, '<u>$1</u>')
   formatted = formatted.replace(/\\n/g, '<br>')
 
   return formatted;
